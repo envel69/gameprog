cc823f1baf8ff1efe2ccb0b77d5ddf6cdf599acb
Righini <envel.righini@gmail.com>
2024-05-28 14:43:50 +0200

    fin de jeu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,47 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
-
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public Ball ball;
-
     public Player player;
     public Player ordi;
-
     public TextMeshProUGUI PlayerScoreText;
     public TextMeshProUGUI OrdiScoreText;
-
-
     public int _scoreplayer;
     public int _scoreordi;
+
+    private const int MaxScore = 3; // Score maximum pour finir le jeu
 
     public void PlayerScores()
     {
         _scoreplayer++;
-
-        this.PlayerScoreText.text = _scoreplayer.ToString();
-        this.player.ResetPosition();
-        this.ordi.ResetPosition();
-        this.ball.ResetPosition();
-        this.ball.AddStartingForce();
-
-        
+        PlayerScoreText.text = _scoreplayer.ToString();
+        CheckGameOver();
+        if (_scoreplayer < MaxScore)
+        {
+            ResetGame();
+        }
     }
 
     public void OrdiScores()
     {
         _scoreordi++;
-
-        this.OrdiScoreText.text = _scoreordi.ToString();
-        this.player.ResetPosition();
-        this.ordi.ResetPosition();
-        this.ball.ResetPosition();
-        this.ball.AddStartingForce();
+        OrdiScoreText.text = _scoreordi.ToString();
+        CheckGameOver();
+        if (_scoreordi < MaxScore)
+        {
+            ResetGame();
+        }
     }
 
-    
+    private void ResetGame()
+    {
+        player.ResetPosition();
+        ordi.ResetPosition();
+        ball.ResetPosition();
+        ball.AddStartingForce();
+    }
 
+    private void CheckGameOver()
+    {
+        if (_scoreplayer >= MaxScore || _scoreordi >= MaxScore)
+        {
+            Debug.Log("Game over reached. Returning to menu.");
+            ReturnToMenu();
+        }
+    }
+
+    private void ReturnToMenu()
+    {
+        Debug.Log("Loading menu scene...");
+        // Load the menu scene. Replace "MenuScene" with the actual name of your menu scene.
+        SceneManager.LoadScene("Menu");
+    }
 }
